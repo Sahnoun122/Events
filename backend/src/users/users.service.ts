9bc65159194228b8b0b57b8558664a06808b6dd9
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-03 11:32:01 +0100

    feat: ajouter le model et services pour user

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User, UserDocument} from './users.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(
+
+  @InjectModel(User.name)
+   private userModel: Model<UserDocument>
+
+){}
+
+create(data : Partial<User>){
+  return this.userModel.create(data);
+}
+
+findByEmail(email : string){
+  return this.userModel.findOne({email});
+}
+}

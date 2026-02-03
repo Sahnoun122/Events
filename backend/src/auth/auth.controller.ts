d1480fedeebf0d9d89d8280b09d736bb1dc7320d
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-03 11:45:56 +0100

    feat: ajouter le model et controller pour l'authentifications

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(
+        private authService : AuthService
+    ){}
+
+    @Post("register")
+    register(@Body() body : any){
+        return this.authService.register(body);
+    }
+
+    @Post("login")
+     login(@Body() body: any){
+        return this.authService.login(body.email , body.password)
+     }
 }

7eedbdd87fb0978be6e8f172b25278891aab8472
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-04 14:19:28 +0100

    feat: la creations de l'architecture pour la reervations et enums

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { EventsModule } from './events/events.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 
 @Module({
@@ -11,6 +12,7 @@ import { EventsModule } from './events/events.module';
     MongooseModule.forRoot('mongodb://localhost:27017/events'),
     AuthModule,
     EventsModule,
+    ReservationsModule,
    
   ],
 

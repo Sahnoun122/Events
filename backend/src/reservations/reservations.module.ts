9830cf706b40e2f2be931523dd9fd0b140478f25
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-04 14:53:51 +0100

    feat: la creations de la model pour la reservations

--- backend/src/reservations/reservations.module.ts
+++ backend/src/reservations/reservations.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ReservationsService } from './reservations.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+import { Reservation , ReservationSchema } from './reservation.schema';
+import { EventsModule } from '../events/events.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Reservation.name, schema: ReservationSchema },
+    ]),
+    EventsModule,
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService],
+  exports: [ReservationsService],
 })
 export class ReservationsModule {}

d2f0c05b9af4248a57619a2e2312f22567f1b7fd
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-04 11:14:10 +0100

    feat: ajouter la structures de gestions des evenments et shema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/events'),
     AuthModule,
+    EventsModule,
    
   ],
 

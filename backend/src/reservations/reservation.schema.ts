e9db59bcdb0986f2bac1c412af736cbc25bff003
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-04 14:22:57 +0100

    feat: la creations de la shema de reservations et dto

---
+++ backend/src/reservations/reservation.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { ReservationStatus } from 'src/common/enums/reservation-status.enum';
+export type ReservationDocument = Reservation & Document;
+
+@Schema({ timestamps: true })
+export class Reservation {
+  @Prop({ type: Types.ObjectId, ref: 'Event', required: true })
+  event: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  participant: Types.ObjectId;
+
+  @Prop({
+    type: String,
+    enum: ReservationStatus,
+    default: ReservationStatus.PENDING,
+  })
+  status: ReservationStatus;
+
+  @Prop({ type: String, maxlength: 300 })
+  comment?: string;
+}
+
+export const ReservationSchema = SchemaFactory.createForClass(Reservation);

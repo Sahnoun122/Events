2a578cbdb144824c62f2041acde106e27d7f7401
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-04 11:20:38 +0100

    feat: ajputer le controller pour gestions des events

--- backend/src/common/decorators/roles.decorator.ts
+++ backend/src/common/decorators/roles.decorator.ts
@@ -2,5 +2,5 @@ import { SetMetadata } from "@nestjs/common";
 import { Role } from "../enums/role.enum";
 
 export const ROLES_KEY = "roles";
-export const roles = (...roles : Role[])=>
+export const Roles = (...roles : Role[])=>
   SetMetadata(ROLES_KEY , roles)
--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,7 +1,74 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
+
+  @Get('public')
+  findAllPublic() {
+    return this.eventsService.findAllPublic();
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get()
+  findAllAdmin() {
+    return this.eventsService.findAllAdmin();
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Post()
+  create(@Body() dto: CreateEventDto) {
+    return this.eventsService.create(dto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventsService.findById(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateEventDto) {
+    return this.eventsService.update(id, dto);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Patch(':id/publish')
+  publish(@Param('id') id: string) {
+    return this.eventsService.publish(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Patch(':id/cancel')
+  cancel(@Param('id') id: string) {
+    return this.eventsService.cancel(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventsService.remove(id);
+  }
 }
